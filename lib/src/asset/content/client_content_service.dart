6518956af9bbaadde30c01d8a3a1b811b0a820d1
ilikerobots <mike@hoolehan.com>
2017-01-10 15:23:57 +0200

    Implement cilent content service

---
+++ lib/src/asset/content/client_content_service.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:angular2/core.dart';
+import 'package:http/http.dart';
+import 'package:path/path.dart' as path;
+
+import 'package:angular2_dart_asset_service/src/asset/content/content_service.dart';
+
+
+const OpaqueToken contentUrl = const OpaqueToken('app.content_url');
+
+@Injectable()
+class ClientContentService implements ContentService {
+
+  final Client _http;
+  final String _contentUrl;
+
+  ClientContentService(@Inject(contentUrl) this._contentUrl, this._http);
+
+  @override
+  Future<String> getContent(String id) async {
+    final String url = path.join(_contentUrl, "$id.html");
+    try {
+      final Response response = await _http.get(url);
+      return response.body;
+    } catch (e) {
+      _handleError(url, e.runtimeType);
+      return "<h3>Error</h3><p>Failed to locate content at $url</p>";
+    }
+  }
+
+  void _handleError(String url, dynamic e) {
+    //TODO: something useful with an error 
+  }
+
+
+}
--- web/main.dart
+++ web/main.dart
@@ -1,17 +1,25 @@
 @MirrorsUsed(override: '*')
 import 'dart:mirrors'; //mirrors not used elsewhere, explicitly discard all symbols
 
+import 'package:http/http.dart';
+import 'package:http/browser_client.dart';
 import 'package:angular2/core.dart';
 import 'package:angular2/platform/browser.dart';
 import 'package:angular2_dart_asset_service/app_component.dart';
 import 'package:angular2_dart_asset_service/src/asset/content/content_service.dart';
-import 'package:angular2_dart_asset_service/src/asset/content/placeholder_content_service.dart';
+
+import 'package:angular2_dart_asset_service/src/asset/content/client_content_service.dart';
+
 
 void main() {
-  bootstrap(AppComponent, <Provider>[
-    provide(ContentService, useClass: PlaceholderContentService)
-  ]);
+  final dynamic contentServiceProvider = ClientContentService;
 
+  bootstrap(AppComponent, <Provider>[
+    provide(ContentService, useClass: ClientContentService),
+    provide(Client, useFactory: () => new BrowserClient(), deps: <Object>[]),
+    provide(contentUrl, useValue: "http://localhost:8090/content/article")
+  ]
+  );
 }
 
 
